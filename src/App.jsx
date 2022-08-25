ef645a2fc5a8ab20970fa128517720045b56ff18
Pedro Santa Barbara <pedrosantb@gmail.com>
2022-08-25 19:10:57 -0300

    First Module

---
+++ src/App.jsx
@@ -0,0 +1,6 @@
+import './styles/global.scss'
+export function App(){
+
+    // throw new Error('Eita Giovana o fornin caiu');
+    return <h1>Hello World</h1>
+}
---
+++ src/index.jsx
@@ -0,0 +1,8 @@
+import { createRoot } from 'react-dom';
+import { App } from './App'
+
+
+const container = document.getElementById('root');
+const root = createRoot(container);
+
+root.render(<App />);
